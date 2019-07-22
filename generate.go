0400f04437e358b721ab240dd6e9af62a9fb3875
nekketsuuu <nekketsuuu@users.noreply.github.com>
2019-07-23 05:40:04 +0900

    fix: read yaml in order on toplevel

--- generate.go
+++ generate.go
@@ -19,46 +19,70 @@ var (
 	indexHTML = filepath.Join("docs", "index.html")
 )
 
-// MapSlice is a wrapper of yaml.MapSlice
-type MapSlice yaml.MapSlice
-
-// HasKey checks whether the slice contains a key named `name`
-func (ms MapSlice) HasKey(name string) bool {
-	for _, mi := range ms {
-		if key, ok := mi.Key.(string); ok && key == name {
-			return true
-		}
+// dict converts (key1, value1, key2, value2, ...) into a dictionary {key1: value1, key2: value2, ...}.
+func dict(values ...interface{}) (map[string]interface{}, error) {
+	// The following implementation is originally written by tux21b on StackOverflow
+	// https://stackoverflow.com/a/18276968/5989200
+	if len(values)%2 != 0 {
+		return nil, errors.New("dict: number of arguments must be even")
 	}
-	return false
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict: keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
 }
 
-// Find returns a value of a key `key` if exists.
+// hasKey checks whether the slice contains a key named `name`.
+func hasKey(msi interface{}, name string) (bool, error) {
+	ms, ok := msi.(yaml.MapSlice)
+	if !ok {
+		return false, errors.New("hasKey: cannot cast to yaml.MapSlice")
+	}
+	for _, mi := range ms {
+		if key, ok := mi.Key.(string); ok && key == name {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// find returns a value of a key `key` if exists.
 // Otherwise, it throws an error.
-func (ms MapSlice) Find(key string) (interface{}, error) {
+func find(msi interface{}, key string) (interface{}, error) {
+	ms, ok := msi.(yaml.MapSlice)
+	if !ok {
+		return nil, errors.New("find: cannot cast to yaml.MapSlice")
+	}
 	for _, mi := range ms {
 		if k, ok := mi.Key.(string); ok && k == key {
 			return mi.Value, nil
 		}
 	}
-	return nil, errors.New("MapSlice.Find: cannot find a key '" + key + "'")
+	return nil, errors.New("find: cannot find a key '" + key + "'")
 }
 
-// IsSingleValue returns false if a value of a key `key` is a slice or an array.
+// isSingleValue returns false if a value of a key `key` is a slice or an array.
 // If it's not, then returns true.
 // If the key doesn't exist, IsSingleValue throws an error.
-func (ms MapSlice) IsSingleValue(key string) (bool, error) {
-	val, err := ms.Find(key)
+func isSingleValue(msi interface{}, key string) (bool, error) {
+	ms, ok := msi.(yaml.MapSlice)
+	if !ok {
+		return false, errors.New("isSingleValue: cannot cast to yaml.MapSlice")
+	}
+	val, err := find(ms, key)
 	if err != nil {
-		return false, errors.Wrap(err, "MapSlice.IsSingleValue: ")
+		return false, errors.Wrap(err, "isSingleValue: cannot find a key")
 	}
 	if k := reflect.ValueOf(val).Kind(); k == reflect.Slice || k == reflect.Array {
 		return false, nil
 	}
 	return true, nil
 }
-
-// DB represents the structure of `data.yml`.
-type DB map[string]MapSlice
 
 func check(err error) {
 	if err != nil {
@@ -69,7 +93,7 @@ func check(err error) {
 func main() {
 	data, err := ioutil.ReadFile(dataYml)
 	check(err)
-	db := make(DB)
+	db := yaml.MapSlice{}
 	err = yaml.Unmarshal(data, &db)
 	check(err)
 
@@ -81,22 +105,10 @@ func main() {
 		"noescape": func(html string) template.HTML {
 			return template.HTML(html)
 		},
-		// The following function is originally written by tux21b on StackOverflow
-		// https://stackoverflow.com/a/18276968/5989200
-		"dict": func(values ...interface{}) (map[string]interface{}, error) {
-			if len(values)%2 != 0 {
-				return nil, errors.New("dict: number of arguments must be even")
-			}
-			dict := make(map[string]interface{}, len(values)/2)
-			for i := 0; i < len(values); i += 2 {
-				key, ok := values[i].(string)
-				if !ok {
-					return nil, errors.New("dict: keys must be strings")
-				}
-				dict[key] = values[i+1]
-			}
-			return dict, nil
-		},
+		"dict":          dict,
+		"hasKey":        hasKey,
+		"find":          find,
+		"isSingleValue": isSingleValue,
 	}
 	tpl, err := template.New(filepath.Base(indexTpl)).Funcs(funcs).ParseFiles(indexTpl, defsTpl)
 	check(err)
